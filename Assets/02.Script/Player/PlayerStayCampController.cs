0cf7a77bc7af8abf23427ba7088dfc2b3ed9562c
Lee <Default.kei072@gmail.com>
2021-05-21 16:23:46 +0900

    キャンプでの移動追加

---
+++ Assets/02.Script/Player/PlayerStayCampController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStayCampController : PlayerController
+{
+    private bool isStageSelecterTouch;
+
+    protected override void InitializeAwkeStep()
+    {
+        base.InitializeAwkeStep();
+
+        // 子で必要な初期化を行う。
+        isStageSelecterTouch = false;
+    }
+
+    protected override void GetUserFKeyInput()
+    {
+        if (!(Input.GetKeyUp(KeyCode.F)))
+        {
+            return;
+        }
+
+        // 処理条件
+
+
+        // 処理内容
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
